d1807804c816879b5b3a483993aec4dc69baa74b
abzno1 <abzno1@bd612c54-c416-11de-9797-47652b86970f>
2011-01-02 00:22:20 +0000

    Restructuring project to allow example module to be created showcasing how mutable passwords can be used.

---
+++ branches/project_restructure/src/test/java/abid/password/type/TimeLockPasswordTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2010 Abid Khalil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package abid.password.type;
+
+import java.util.Calendar;
+
+import junit.framework.TestCase;
+import abid.password.MutableBlock;
+import abid.password.MutablePassword;
+import abid.password.PasswordException;
+import abid.password.parameters.TimeParameter;
+import abid.password.types.TimeLockPassword;
+
+public class TimeLockPasswordTest extends TestCase {
+
+  public void testTimeLockPassword() throws PasswordException {
+    String confirmPassword = "abid";
+    MutablePassword mutatingPassword = TimeLockPassword.createPassword("abid", TimeParameter.HOUR, 0, 24);
+    assertEquals(true, mutatingPassword.confirmPassword(confirmPassword));
+
+    MutablePassword mutatingPassword2 = TimeLockPassword.createPassword("abid", TimeParameter.HOUR, -2, -1);
+    assertEquals(false, mutatingPassword2.confirmPassword(confirmPassword));
+
+    String wrongPassword = "diba";
+    MutablePassword mutatingPassword3 = TimeLockPassword.createPassword("abid", TimeParameter.HOUR, 0, 24);
+    assertEquals(false, mutatingPassword3.confirmPassword(wrongPassword));
+  }
+
+  public void testDayOfWeekPassword() throws PasswordException {
+    // Calendar.SUNDAY = 1
+    // Calendar.MONDAY = 2
+    // Calendar.TUESDAY = 3
+    // Calendar.WEDNESDAY = 4
+    // Calendar.THURSDAY = 5
+    // Calendar.FRIDAY = 6
+    // Calendar.SATURDAY = 7
+    String confirmPassword = "a";
+    String wrongPassword = "wrongDayOfWeek";
+    MutablePassword mutatingPassword = TimeLockPassword.createPassword(confirmPassword, TimeParameter.DAY_OF_WEEK, Calendar.SUNDAY, Calendar.SATURDAY);
+    assertEquals(true, mutatingPassword.confirmPassword(confirmPassword));
+    assertEquals(false, mutatingPassword.confirmPassword(wrongPassword));
+  }
+
+  public void testPasswordException() {
+    String passwordText = "abid";
+    String confirmPassword = "abid";
+    MutableBlock mutableBlock = NewTimeLockPassword.createMutableBlock(TimeParameter.HOUR, "a", "b");
+    MutablePassword mutatingPassword = new TimeLockPassword(passwordText, mutableBlock);
+
+    try {
+      mutatingPassword.confirmPassword(confirmPassword);
+    } catch (PasswordException e) {
+      return;
+    }
+    fail("Should not read this!");
+  }
+}
---
+++ branches/project_restructure/src/test/java/abid/password/util/StreamManagementTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2010 Abid Khalil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package abid.password.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.TestCase;
+
+public class StreamManagementTest extends TestCase {
+
+  public void testStreamToString() throws IOException {
+    String testStr = "test";
+    InputStream in = new ByteArrayInputStream(testStr.getBytes());
+    String convertedStr = StreamManagement.convertStreamToString(in);
+    in.close();
+    assertEquals(testStr, convertedStr);
+  }
+
+  /*
+   * Workaround to get 100% coverage on a private constructor.
+   */
+  public void testStream() throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    final Class<?> cls = StreamManagement.class;
+    final Constructor<?> c = cls.getDeclaredConstructors()[0];
+    c.setAccessible(true);
+    final Object n = c.newInstance((Object[]) null);
+  }
+
+}
